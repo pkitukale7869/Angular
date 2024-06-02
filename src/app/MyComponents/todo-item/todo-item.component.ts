a5cb177fb09204c534866addaa61e65071f22207
pkitukale7869 <pkitukale7869@gmail.com>
2024-06-02 20:22:41 +0530

    Add of  Checkbox

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -1,23 +1,25 @@
-import { Component, Input, OnInit,Output,EventEmitter  } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Todo } from '../../Todo';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
-  styleUrl: './todo-item.component.css'
+  styleUrl: './todo-item.component.css',
 })
-export class TodoItemComponent implements OnInit{
+export class TodoItemComponent implements OnInit {
+  onCheckBoxClick(todo: Todo) {
+    this.todoCheckBox.emit(todo);
+  }
   @Input() todo!: Todo;
+  @Input() i!: number;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
-  constructor(){
-    
-  }
-  ngOnInit(): void {
-  }
+  @Output() todoCheckBox: EventEmitter<Todo> = new EventEmitter();
+  constructor() {}
+  ngOnInit(): void {}
   onclick(todo: Todo) {
     this.todoDelete.emit(todo);
-    console.log("Deleted the event")
+    console.log('Deleted the event');
   }
-
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -7,45 +7,41 @@ import { AddTodoComponent } from '../add-todo/add-todo.component';
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule,TodoItemComponent,AddTodoComponent],
+  imports: [CommonModule, TodoItemComponent, AddTodoComponent],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
 export class TodosComponent implements OnInit {
-  todos:Todo[];
-  constructor()
-  { 
-     this.todos=
-     [
-        {
-          sno: 1,
-          title: 'This is Title_1',
-          desc: 'Description',
-          active: true
-        },
-        {
-          sno: 2,
-          title: 'This is Title_2',
-          desc: 'Description',
-          active: true
-        },
-        {
-          sno: 3,
-          title: 'This is Title_3',
-          desc: 'Description',
-          active: false
-        }
-      ]
+  todos: Todo[] = [];
+  localItem: string | null | undefined;
+  constructor() {
+    if (typeof localStorage !== 'undefined') {
+      this.localItem = localStorage.getItem('todos');
+      if (this.localItem == null) {
+        this.todos = [];
+      } else {
+        this.todos = JSON.parse(this.localItem);
+      }
+    } else {
+      this.todos = []; // Fallback in case localStorage is not available
+    }
   }
-  ngOnInit(): void {
-  }
-  deleteToDo(todo:Todo){
+  ngOnInit(): void {}
+  deleteToDo(todo: Todo) {
     console.log(todo);
     const index = this.todos.indexOf(todo);
-    this.todos.splice(index,1);
+    this.todos.splice(index, 1);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
-  addToDo(todo:Todo){
+  addToDo(todo: Todo) {
     console.log(todo);
     this.todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+  toggleToDo(todo: Todo) {
+    console.log(todo);
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active = !this.todos[index].active;
+    localStorage.setItem('todos', JSON.stringify(this.todos));
   }
 }
